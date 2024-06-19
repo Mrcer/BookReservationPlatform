e681b1ddeb4c02ce57bceb62b4c476d0c51e3875
huangmk <2518192202@qq.com>
2024-06-19 14:51:27 +0800

    完成register外观

--- client/src/components/Login/Register.vue
+++ client/src/components/Login/Register.vue
@@ -1,8 +1,38 @@
-2
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const registerForm = ref({
+  net_id: '',
+  name: '',
+  password: '',
+})
+
+const handleRegister = () => {
+  console.log(registerForm.value)
+}
+</script>
 
 <template>
-  <h1>Register</h1>
+  <div>
+    <el-form :model="registerForm" label-width="100px">
+      <el-form-item label="NetID">
+        <el-input v-model="registerForm.net_id" placeholder="Please enter your NetID" />
+      </el-form-item>
+      <el-form-item label="Name">
+        <el-input v-model="registerForm.name" placeholder="Please enter your name" />
+      </el-form-item>      
+      <el-form-item label="Password">
+        <el-input
+          v-model="registerForm.password"
+          type="password"
+          placeholder="Please enter your password"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handleRegister">Create</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <style scoped></style>
