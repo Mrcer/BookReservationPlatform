ef05e76cd8ca9cfd139f54fa8e8a2007f66b4d83
huangmk <2518192202@qq.com>
2024-06-23 18:17:18 +0800

    注册逻辑

--- client/src/components/Login/Register.vue
+++ client/src/components/Login/Register.vue
@@ -1,25 +1,49 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import {createUser} from '../../service/user'
+import { ElMessageBox } from 'element-plus'
 
 const registerForm = ref({
-  net_id: '',
-  name: '',
-  password: '',
+  username:'', 
+  password:'', 
+  email:'', 
+  role:'',
 })
 
+const message = ref('')
+
 const handleRegister = () => {
-  console.log(registerForm.value)
+
+  createUser(registerForm)
+  .then((res)=>{
+    if(res.data.status === 201){
+      message.value = res.data.value.message
+    }
+    else if(res.data.status === 400){
+      message.value = res.data.value.error
+    }
+    else{
+      console.log("Unknown status code")
+    }
+  })
+  .catch((err)=>{console.log(err)})
+
+  ElMessageBox.alert(message.value, 'notion', {
+    // if you want to disable its autofocus
+    // autofocus: false,
+    confirmButtonText: 'OK',
+  })
 }
 </script>
 
 <template>
   <div>
     <el-form :model="registerForm" label-width="100px">
-      <el-form-item label="NetID">
-        <el-input v-model="registerForm.net_id" placeholder="Please enter your NetID" />
+      <el-form-item label="username">
+        <el-input v-model="registerForm.username" placeholder="Please enter your username" />
       </el-form-item>
-      <el-form-item label="Name">
-        <el-input v-model="registerForm.name" placeholder="Please enter your name" />
+      <el-form-item label="email">
+        <el-input v-model="registerForm.email" placeholder="Please enter your email" />
       </el-form-item>
       <el-form-item label="Password">
         <el-input
