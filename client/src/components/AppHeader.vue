45f9149d975dfe9ab5d33eabf738d8b357b10c0f
Mrcer <2969944867@qq.com>
2024-06-24 19:08:27 +0800

    fix: 从profile切换到reserve的路由错误

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -16,11 +16,11 @@ const userStore = useUserStore()
           <HomeFilled />
         </el-icon>
       </RouterLink>
-      <el-menu class="menu" mode="horizontal" router text-color="#fff" active-text-color="#fff">
-        <el-menu-item index="reserve" v-if="userStore.isLoggedIn">预约板块</el-menu-item>
-        <el-menu-item index="profile" v-if="userStore.isLoggedIn">用户主页</el-menu-item>
+      <el-menu class="menu" mode="horizontal" text-color="#fff" active-text-color="#fff">
+        <el-menu-item index="reserve" v-if="userStore.isLoggedIn" @click="router.push('/reserve')">预约板块</el-menu-item>
+        <el-menu-item index="profile" v-if="userStore.isLoggedIn" @click="router.push('/profile')">用户主页</el-menu-item>
         <!-- TODO: admin auth -->
-        <el-menu-item index="admin" v-if="false">管理页面</el-menu-item>
+        <el-menu-item index="admin" v-if="false" @click="router.push('/admin')">管理页面</el-menu-item>
       </el-menu>
       <div class="user-info" v-if="userStore.isLoggedIn">
         <span>{{ userStore.username }}</span>
